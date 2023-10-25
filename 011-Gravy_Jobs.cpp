9d03b088407d933445d098fd10b18e8ba6c46e61
NaoyukiKita <KitaNaoyuki99@gmail.com>
2023-10-25 12:06:22 +0900

    solve 011

--- 011-Gravy_Jobs.cpp
+++ 011-Gravy_Jobs.cpp
@@ -25,33 +25,28 @@ int main() {
     int N, D, C, S;
     cin >> N;
 
-    vector<Task> tasks(N);
-    int max_len = 0;
-    for (int i = 0; i < N; i++) {
+    vector<Task> tasks(N + 1);
+    for (int i = 1; i <= N; i++) {
         cin >> D >> C >> S;
         tasks[i] = initTask(D, C, S);
-        if (D <= max_len) continue;
-        max_len = D;
     }
 
     sort(tasks.begin(), tasks.end());
 
-    vector<vector <ull> > dp(N + 1, vector<ull>(max_len + 1));
+    int max_due = tasks[N].due;
+    vector<vector <ull> > dp(N + 1, vector<ull>(max_due + 1));
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < max_len; j++) {
-            if (j + tasks[i].len > tasks[i].due) break;
-            dp[i + 1][j + tasks[i].len] += dp[i][j] + tasks[i].praise;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j <= max_due; j++) {
+            if (j < tasks[i].len || tasks[i].due < j) {
+                dp[i][j] = dp[i - 1][j];
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - tasks[i].len] + tasks[i].praise);
+            }
         }
     }
 
-    cout << dp[N][max_len] << endl;
-
-    // cout << "-----------------\n";
-    // for (int i = 0; i <= N; i++) {
-    //     for (int j = 0; j <= max_len; j++) {
-    //         cout << dp[i][j] << ' ';
-    //     }
-    //     cout << endl;
-    // }
+    ull max_val = 0;
+    for (int j = 0; j <= max_due; j++) max_val = max_val > dp[N][j] ? max_val : dp[N][j];
+    cout << max_val << endl;
 }
