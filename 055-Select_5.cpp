a03480680a61e48b47be43d51e46e8f920bcfd61
NaoyukiKita <KitaNaoyuki99@gmail.com>
2024-01-11 18:25:51 +0900

    solve 055

---
+++ 055-Select_5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long int;
+
+ll func(vector<int>& A, int begin, ll current, int counter, int target, int mod) {
+    // cout << "func(A, " << begin << ", " << current << ", " << counter << ", " << target << ", " << mod << ")\n";
+    if (counter == 0) return current == target;
+    
+    ll sum = 0;
+    for (int i = begin; i < A.size(); i++) {
+        sum += func(A, i+1, (current*A[i]) % mod, counter-1, target, mod);
+    }
+    
+    return sum;
+}
+
+int main() {
+    int N, P, Q;
+    cin >> N >> P >> Q;
+    
+    vector<int> A(N);
+    for (int& a : A) cin >> a;
+    
+    cout << func(A, 0, 1, 5, Q, P) << endl;
+}
